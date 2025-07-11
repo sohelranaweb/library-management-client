5e2d9cf8327f22b8109a0ab5f1047cc75a08f8d8
sohelranaweb <sohel.rana.web2@gmail.com>
2025-07-11 17:38:53 +0600

    borrow summary loading added

--- src/pages/BorrowSummary/BorrowSummary.tsx
+++ src/pages/BorrowSummary/BorrowSummary.tsx
@@ -1,15 +1,20 @@
 import BorrowSummaryTable from "@/components/BorrowSummaryTable";
 import { useGetBorrowSummaryQuery } from "@/redux/api/bookApi";
 
-
 const BorrowSummary = () => {
-  const { data: borrows} = useGetBorrowSummaryQuery(undefined);
+  const { data: borrows, isLoading } = useGetBorrowSummaryQuery(undefined);
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+    );
   const borrowData = borrows?.data;
   return (
     <div className="max-w-6xl mx-auto p-4">
       <BorrowSummaryTable borrowSummary={borrowData}></BorrowSummaryTable>
     </div>
-  )
-}
+  );
+};
 
-export default BorrowSummary
+export default BorrowSummary;
--- src/redux/api/bookApi.ts
+++ src/redux/api/bookApi.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const bookApi = createApi({
   reducerPath: "bookApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://library-management-topaz-rho.vercel.app/api",
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://library-management-topaz-rho.vercel.app/api",
+    // baseUrl: "http://localhost:5000/api",
   }),
   tagTypes: ["book"],
   endpoints: (builder) => ({
